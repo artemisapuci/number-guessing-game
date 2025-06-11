6c5f2a83041d2ccb9676434be4f59b22bca3ba15
Artemisa Puci <artemisa.puci@gmail.com>
2025-06-11 13:45:28 +0000

    chore: implement random number generation and guessing loop

--- number_guess.sh
+++ number_guess.sh
@@ -17,3 +17,31 @@ else
   IFS="|" read DB_USERNAME GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
   echo "Welcome back, $DB_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# Generate secret number
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+echo "Guess the secret number between 1 and 1000:"
+
+NUMBER_OF_GUESSES=0
+
+while true; do
+  read GUESS
+  ((NUMBER_OF_GUESSES++))
+
+  # Check if input is an integer
+  if ! [[ "$GUESS" =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  if (( GUESS < SECRET_NUMBER )); then
+    echo "It's higher than that, guess again:"
+  elif (( GUESS > SECRET_NUMBER )); then
+    echo "It's lower than that, guess again:"
+  else
+    break
+  fi
+done
+
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
